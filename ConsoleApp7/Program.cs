ad02ce1dfb51d1560f52752c281228bf315be820
NHOY <NHOY@2210DR000919>
2023-05-03 13:30:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInAlbum = 52;
+            int maximumPicturesInSeries = 3;
+            int fullyFilledSerises = picturesInAlbum / maximumPicturesInSeries;
+            int incompleteSeries = picturesInAlbum % maximumPicturesInSeries;
+            Console.WriteLine(fullyFilledSerises);
+            Console.WriteLine(incompleteSeries);    
+        }
+    }
+}
